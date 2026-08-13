5d2ca371d10b16827133aa309a385cc66b645abb
Jeff Lewis <mail@jeffreylewis.me>
2026-08-13 14:32:11 -0400

    fix: use repaired bundled SQLite

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
-    .package(url: "https://github.com/jefflewis/GRDB.swift", revision: "8379977"),
+    .package(url: "https://github.com/jefflewis/GRDB.swift", revision: "d5f4008"),
     .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.4.0"),
     .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.3.3"),
     .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.9.0"),
